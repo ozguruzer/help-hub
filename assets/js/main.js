1d1335847f3145101546f3c95e717f0fe890b94e
Ozgur <ozguruzer98@gmail.com>
2023-02-08 17:10:06 +0300

    responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,18 +6,29 @@ loop: true,
 
 
 
-var swiper = new Swiper(".cauSwiper", {
-slidesPerView: 3,
-spaceBetween: 30,
+var swiper = new Swiper(".cauSwiper .swiper-container", {
+slidesPerView: 1,
 slidesPerGroup: 1,
-loop: true,
-loopFillGroupWithBlank: true,
-pagination: {
-el: ".swiper-pagination",
-clickable: true,
-},
+spaceBetween: 10,
+loop: false,
 navigation: {
-nextEl: ".swiper-button-next",
-prevEl: ".swiper-button-prev",
+  nextEl: ".cauSwiper .swiper-button-next",
+  prevEl: ".cauSwiper .swiper-button-prev",
+},
+breakpoints: {
+  767: {
+    slidesPerView: 2,
+    slidesPerGroup: 2,
+    spaceBetween: 20
+  },
+  993: {
+    slidesPerView: 3,
+    slidesPerGroup: 3,
+    spaceBetween: 30
+  }
+},
+pagination: {
+  el: ".cauSwiper .swiper-pagination",
+  clickable: true,
 },
 });
